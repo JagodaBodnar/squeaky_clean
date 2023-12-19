c021ef556dba5d9ece5b1ccc5c5b783a1317c983
JagodaBodnar <jagodabodnar90@gmail.com>
2023-12-19 01:02:28 +0100

    SqueakyClean exercise.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Exercise from  https://exercism.org/tracks/java/exercises/squeaky-clean
 
 1. Replace any spaces encountered with underscores
-   Implement the (static) SqueakyClean.clean() method to replace any spaces with underscores. This also applies to leading and trailing spaces.
+    Implement the (static) SqueakyClean.clean() method to replace any spaces with underscores. This also applies to leading and trailing spaces.
 2. Replace control characters with the upper case string "CTRL"
       Modify the (static) SqueakyClean.clean() method to replace control characters with the upper case string "CTRL".
-3. SqueakyClean.clean("à-ḃç");
-   // => "àḂç"
+3. Convert kebab-case to camelCase
+      Modify the (static) SqueakyClean.clean() method to convert kebab-case to camelCase.
 4. Modify the (static) SqueakyClean.clean() method to omit any characters that are not letters.
 5. Omit Greek lower case letters
       Modify the (static) SqueakyClean.clean() method to omit any Greek letters in the range 'α' to 'ω'.
--- src/SqueakyClean.java
+++ src/SqueakyClean.java
@@ -1,9 +1,15 @@
 public class SqueakyClean {
     public static void main(String[] args) {
         String cleanedString = SqueakyClean.cleanSpaces("my   Id");
-        System.out.printf("Encountered spaces has been replaced with underscores: %s%n",cleanedString);
+        System.out.printf("Replace any spaces encountered with underscores: %s%n",cleanedString);
         cleanedString = SqueakyClean.cleanControlChars("my\0Id");
-        System.out.printf("Encountered spaces has been replaced with underscores: %s%n",cleanedString);
+        System.out.printf("Replace control characters with the upper case string \"CTRL\": %s%n",cleanedString);
+        cleanedString = SqueakyClean.cleanKebabCase("à-ḃç");
+        System.out.printf("Convert kebab-case to camelCase: %s%n",cleanedString);
+        cleanedString = SqueakyClean.cleanNotLetters("a1😀2😀3😀b");
+        System.out.printf("Omit characters that are not letters: %s%n",cleanedString);
+        cleanedString = SqueakyClean.cleanOmitGreekLowerCaseLetters("MyΟβιεγτFinder");
+        System.out.printf("Omit Greek lower case letters: %s%n",cleanedString);
     }
     public static String cleanSpaces(String string){
         StringBuilder transformString = new StringBuilder(string);
@@ -18,6 +24,40 @@ public class SqueakyClean {
 
     public static String cleanControlChars(String string){
         return string.replaceAll("\0", "CTRL");
+    }
+
+    public static String cleanKebabCase(String string){
+        StringBuilder transformString = new StringBuilder(string);
+        String[] splitString =  string.split("");
+        for(int i=0; i < string.length(); i++ ){
+            if(splitString[i].equals("-")){
+                transformString.replace(i, i+2, splitString[i+1].toUpperCase());
+            }
+        }
+        return transformString.toString();
+    }
+
+    public static String cleanNotLetters(String string){
+        StringBuilder transformString = new StringBuilder();
+        String[] splitString =  string.split("");
+        for(int i=0; i < string.length(); i++ ){
+            if(splitString[i].matches("[a-z, A-Z]")){
+                transformString.append(splitString[i]);
+            }
+        }
+        return transformString.toString();
+    }
+
+    public static String cleanOmitGreekLowerCaseLetters(String string){
+        StringBuilder transformString = new StringBuilder(string);
+        String[] splitString =  string.split("");
+        for(int i=0; i < string.length(); i++){
+            if(splitString[i].matches("[α-ω]")){
+               int index = transformString.indexOf(splitString[i]);
+                transformString.deleteCharAt(index);
+            }
+        }
+        return transformString.toString();
     }
 
 }
